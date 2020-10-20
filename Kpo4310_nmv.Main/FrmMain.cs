259ee5923c2230f5ded3308e67d780e879991c3c
Amir <46008130+MrPrometheus@users.noreply.github.com>
2020-10-20 16:54:54 +0300

    1) Создание нового проекта WinForms
    1) Создан проект. 2) Настроен проект. 3) Настроена главная форма проекта. 4) Написан обработчик кнопки Открыть.

---
+++ Kpo4310_nmv.Main/FrmMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kpo4310_nmv.Main
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void mnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Вызов исключения "Метод не реализован"
+                //throw new NotImplementedException();
+                //Вызов базового исключения
+                throw new Exception("Неправильные входные параметры");
+            }
+            //обработка исключения "Метод не реализован"
+            catch (NotImplementedException ex)
+            {
+                MessageBox.Show("Ошибка №1: " + ex.Message);
+            }
+            //обработка остальных исключений
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка №2: " + ex.Message);
+            }
+
+        }
+    }
+}
