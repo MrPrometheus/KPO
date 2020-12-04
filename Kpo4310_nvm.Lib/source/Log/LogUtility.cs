5e8314791e09084004565c5326857ecc46302192
MrPrometheus <amir505553amir@gmail.com>
2020-12-04 21:23:31 +0300

    fix: гитигнор пофикшен

---
+++ Kpo4310_nvm.Lib/source/Log/LogUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kpo4310_nvm.Lib
+{
+    public class LogUtility
+    {
+        public static void ErrorLog(string message)
+        {
+            File.AppendAllText("error.log", DateTime.Now.ToString("F") + " // " + message + "\n");
+        }
+
+        public static void ErrorLog(Exception ex)
+        {
+            File.AppendAllText("error.log", DateTime.Now.ToString("F") + " // " + ex.Message + "\n");
+        }
+    }
+}
